f9e0801d6f331fb1b9d4870e13ddc17a58d98e8b
Edu Melgaço <eduhdm0105@gmail.com>
2022-05-29 15:57:50 -0300

    Implement other expense types

--- src/expenses/expense.py
+++ src/expenses/expense.py
@@ -7,29 +7,126 @@ class ExpenseTypes(Enum):
   PERCENTAGE = 2
   VALUE = 3
 
-class Expense:
-  id: int
-  paid_by: int
-  used_by: List[int] = []
+class ExpenseBase:
   total_value: float
+  used_by: List[int] = []
   description: str
+  paid_by: int
 
-  def __init__(self, id, paid_by=None, used_by=[], total_value=None, description=None):
-    self.id = id
-    self.paid_by = paid_by
-    self.used_by = used_by
+  def __init__(self, paid_by=None, used_by=[], total_value=None, description=None):
+    if(total_value <= 0):
+      raise Exception('Expense value cannot be negative or zero.')
+
+    if(len(used_by)== 0):
+      raise Exception('Used by cannot be empty.')
+
+    if(type(description) != 'str' ro len(description)== 0):
+      raise Exception('Description must be a valid string.')
+
     self.total_value = total_value
     self.description = description
+    self.paid_by = paid_by
+    self.used_by = used_by
 
-  def get_user_balance(self, user_id):
+  def _assert_valid_user(self, user_id):
     if (user_id != self.paid_by and user_id not in self.used_by):
       raise UserNotFoundException(user_id)
 
-    if (user_id == self.paid_by):
-      if (user_id in self.used_by):
+  def get_user_balance(self, user_id):
+    raise Exception('Not implemented')
+
+class ExpenseEqual(ExpenseBase):
+  def __init__(self, total_value=None, description=None, paid_by=None, used_by=[]):
+    super().__init__(paid_by, used_by, total_value, description)
+
+  def get_user_balance(self, user_id):
+    self._assert_valid_user(user_id)
+
+    if (user_id in self.used_by):
+      if (user_id == self.paid_by):
         return self.total_value * ((len(self.used_by) - 1) / len(self.used_by))
 
-      return self.total_value
+      return - ((1 / len(self.used_by)) * self.total_value)
 
+    return self.total_value
 
-    return - ((1 / len(self.used_by)) * self.total_value)
+class ExpensePercentage(ExpenseBase):
+  percentage_used: List[float] = []
+
+  def __init__(self, total_value=None, description=None, paid_by=[], used_by=[],percentage_used=[]):
+    super().__init__(paid_by, used_by, total_value, description)
+    if(len(used_by) != len(percentage_used)):
+      raise Exception('Used list and percentage list must have the same length.')
+
+    if (sum(percentage_used) != 100):
+      raise Exception('Percentage total should be equal a 100.')
+
+    self.percentage_used = percentage_used
+
+  def get_user_balance(self, user_id):
+    self._assert_valid_user(user_id)
+
+    if (user_id in self.used_by):
+      user_index = self.used_by.index(user_id)
+      user_percentage = self.percentage_used[user_index]
+
+      if (user_id == self.paid_by):
+        return self.total_value - (user_percentage / 100 * self.total_value)
+
+      return - (user_percentage / 100 * self.total_value)
+
+    return self.total_value
+
+class ExpenseValue(ExpenseBase):
+  value_used: List[float] = []
+
+  def __init__(self, total_value=None, description=None, paid_by=[], used_by=[], value_used=[]):
+    super().__init__(paid_by, used_by, total_value, description)
+
+    if(len(used_by) != len(value_used)):
+      raise Exception('Used list and percentage list must have the same length.')
+
+    if (sum(value_used) != self.total_value):
+      raise Exception('Sum of used value must be equal total value.')
+
+    self.value_used = value_used
+
+  def get_user_balance(self, user_id):
+    self._assert_valid_user(user_id)
+
+    if (user_id in self.used_by):
+      user_index = self.used_by.index(user_id)
+      user_used_value = self.value_used[user_index]
+
+      if (user_id == self.paid_by):
+          return self.total_value - user_used_value
+
+      return -user_used_value
+
+    return self.total_value
+
+class Expense:
+  expense: ExpenseBase
+  id: int
+
+  def __init__(self, id, expense_type: ExpenseTypes = None, **kwargs):
+    print(kwargs)
+    self.id = id
+    if(expense_type == ExpenseTypes.EQUAL):
+      self.expense = ExpenseEqual(**kwargs)
+    elif(expense_type == ExpenseTypes.PERCENTAGE):
+      self.expense = ExpensePercentage(**kwargs)
+    elif(expense_type == ExpenseTypes.VALUE):
+      self.expense = ExpenseValue(**kwargs)
+    else:
+      raise Exception("Invalid Expense type.")
+
+  def get_user_balance(self, user_id):
+    return self.expense.get_user_balance(user_id)
+
+  def get_paid_by(self):
+    return self.expense.paid_by
+
+  def get_used_by(self):
+    return self.expense.used_by
+
--- tests/expense_tests.py
+++ tests/expense_tests.py
@@ -1,44 +1,69 @@
 import unittest
 from src.exceptions.exceptions import UserNotFoundException
-from src.expenses.expense import Expense
+from src.expenses.expense import Expense, ExpenseTypes
 
 class TestExpense(unittest.TestCase):
 
     def test_payor_balance_is_positive_when_splitting_in_half(self):
         payor_id = 1
         user_id = 2
-        expense = Expense(id = 1, paid_by = payor_id,
-                used_by = [payor_id, user_id], total_value = 100)
+        expense = Expense(
+            id = 1,
+            expense_type=ExpenseTypes.EQUAL,
+            description='Despesa',
+            paid_by = payor_id,
+            used_by = [payor_id, user_id],
+            total_value = 100)
 
         self.assertTrue(expense.get_user_balance(payor_id) == 50)
 
     def test_payor_balance_is_zero_when_not_splitting(self):
         payor_id = 1
-        expense = Expense(id = 1, paid_by = payor_id, used_by = [1],
-                total_value = 100)
+        expense = Expense(
+            id = 1,
+            expense_type=ExpenseTypes.EQUAL,
+            description='Despesa',
+            paid_by = payor_id,
+            used_by = [payor_id],
+            total_value = 100)
 
         self.assertTrue(expense.get_user_balance(payor_id) == 0)
 
     def test_payor_balance_is_total_when_buying_for_other_user(self):
         payor_id = 1
-        expense = Expense(id = 1, paid_by = payor_id, used_by = [2],
-                total_value = 100)
+        expense = Expense(
+            id = 1,
+            expense_type=ExpenseTypes.EQUAL,
+            description='Despesa',
+            paid_by = payor_id,
+            used_by = [2],
+            total_value = 100)
 
         self.assertTrue(expense.get_user_balance(payor_id) == 100)
 
     def test_user_balance_is_negative_when_not_paying(self):
         payor_id = 1
         user_id = 2
-        expense = Expense(id = 1, paid_by = payor_id,
-                used_by = [payor_id, user_id], total_value = 100)
+        expense = Expense(
+            id = 1,
+            expense_type=ExpenseTypes.EQUAL,
+            description='Despesa',
+            paid_by = payor_id,
+            used_by = [payor_id, user_id],
+            total_value = 100)
 
         self.assertTrue(expense.get_user_balance(user_id) == -50)
 
     def test_get_user_balance_throws_not_found(self):
         payor_id = 1
         user_id = 2
-        expense = Expense(id = 1, paid_by = payor_id,
-                used_by = [payor_id, user_id], total_value = 100)
+        expense = Expense(
+            id = 1,
+            expense_type=ExpenseTypes.EQUAL,
+            description='Despesa',
+            paid_by = payor_id,
+            used_by = [payor_id, user_id],
+            total_value = 100)
 
         with self.assertRaises(UserNotFoundException):
             expense.get_user_balance(user_id = 3)
