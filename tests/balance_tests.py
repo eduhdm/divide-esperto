0dc152a1d6896150652dad20150c398fcde6ada7
Edu Melgaço <eduhdm0105@gmail.com>
2022-05-29 13:56:28 -0300

    Add user not found tests to balance_tests

--- tests/balance_tests.py
+++ tests/balance_tests.py
@@ -1,4 +1,5 @@
 import unittest
+from src.exceptions.exceptions import UserNotFoundException
 from src.expenses.balance import Balance
 
 class TestBalance(unittest.TestCase):
@@ -23,13 +24,6 @@ class TestBalance(unittest.TestCase):
         self.assertTrue(balance.get_user_balance(user_id = 1) == 50)
         self.assertTrue(balance.get_user_balance(user_id = 2) == -50)
 
-    def test_calculate_user_balance_throws_when_user_not_found(self):
-        balance = Balance(user_a = 1, user_b = 2, amount_paid_a = 100,
-                amount_paid_b = 0)
-
-        with self.assertRaises(Exception):
-            balance.get_user_balance(user_id = 3)
-
     def test_counterparty_balance(self):
         balance = Balance(user_a = 1, user_b = 2, amount_paid_a = 100,
                 amount_paid_b = 0)
@@ -38,3 +32,24 @@ class TestBalance(unittest.TestCase):
         counterparty_id = balance.get_counterparty_id(user_id = 1)
 
         self.assertTrue(balance.get_user_balance(user_id = counterparty_id) == -100)
+
+    def test_add_amount_throws_when_user_not_found(self):
+        balance = Balance(user_a = 1, user_b = 2, amount_paid_a = 100,
+                amount_paid_b = 0)
+
+        with self.assertRaises(UserNotFoundException):
+            balance.add_amount(user_id = 3, amount=10)
+
+    def test_calculate_user_balance_throws_when_user_not_found(self):
+        balance = Balance(user_a = 1, user_b = 2, amount_paid_a = 100,
+                amount_paid_b = 0)
+
+        with self.assertRaises(UserNotFoundException):
+            balance.get_user_balance(user_id = 3)
+
+    def test_get_counterparty_id_throws_when_user_not_found(self):
+        balance = Balance(user_a = 1, user_b = 2, amount_paid_a = 100,
+                amount_paid_b = 0)
+
+        with self.assertRaises(UserNotFoundException):
+            balance.get_counterparty_id(user_id = 3)
