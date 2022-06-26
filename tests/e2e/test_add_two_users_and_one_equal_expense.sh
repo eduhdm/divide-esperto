5ef0d77da479042c7f8d1fb9e87faf865e6d6609
Lucas Caetano <lucasclopesr@gmail.com>
2022-06-26 15:32:57 -0300

    Add e2e tests

---
+++ tests/e2e/test_add_two_users_and_one_equal_expense.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# To run these tests, run export PYTHONPATH="/path/to/root/folder"
+
+TEST=$(cat << EOF | python src/main.py
+adduser Lucas
+adduser Eduardo
+addexpense
+equal
+100
+First expense
+1
+1 2
+get_user_balance 1
+EOF
+)
+
+EXPECTED_OUTPUT='You are owed 50.0'
+
+if [[ "$TEST" == *"$EXPECTED_OUTPUT"* ]]; then
+    echo "Output is correct"
+else
+    echo "Output is incorrect"
+    exit 1
+fi
+
+TEST2=$(cat << EOF | python src/main.py
+adduser Lucas
+adduser Eduardo
+addexpense
+equal
+100
+First expense
+1
+1 2
+get_user_balance 2
+EOF
+)
+
+EXPECTED_OUTPUT2='You owe 50.0'
+
+if [[ "$TEST2" == *"$EXPECTED_OUTPUT2"* ]]; then
+    echo "Output is correct"
+else
+    echo "Output is incorrect"
+    exit 1
+fi
+
+
+exit 0
---
+++ tests/e2e/test_add_two_users_and_one_value_expense.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# To run these tests, run export PYTHONPATH="/path/to/root/folder"
+
+TEST=$(cat << EOF | python src/main.py
+adduser Lucas
+adduser Eduardo
+addexpense
+value
+100
+First expense
+1
+1 2
+60 40
+get_user_balance 1
+EOF
+)
+
+EXPECTED_OUTPUT='You are owed 40'
+
+if [[ "$TEST" == *"$EXPECTED_OUTPUT"* ]]; then
+    echo "Output is correct"
+else
+    echo "Output is incorrect"
+    exit 1
+fi
+
+TEST2=$(cat << EOF | python src/main.py
+adduser Lucas
+adduser Eduardo
+addexpense
+value
+100
+First expense
+1
+1 2
+60 40
+get_user_balance 2
+EOF
+)
+
+EXPECTED_OUTPUT2='You owe 40'
+
+if [[ "$TEST2" == *"$EXPECTED_OUTPUT2"* ]]; then
+    echo "Output is correct"
+else
+    echo "Output is incorrect"
+    exit 1
+fi
+
+
+exit 0
